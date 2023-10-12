35abf4312415fc17955fd3bd9f6ef12fa6be5490
mtgag <githreg@mtg.de>
2023-10-12 07:58:28 +0200

    pull request 742 of zlint

---
+++ jzlint/src/main/java/de/mtg/jzlint/lints/cabf_smime_br/SubscribersShallHaveCrlDistributionPoints.java
@@ -0,0 +1,60 @@
+package de.mtg.jzlint.lints.cabf_smime_br;
+
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.x509.CRLDistPoint;
+import org.bouncycastle.asn1.x509.DistributionPoint;
+import org.bouncycastle.asn1.x509.Extension;
+
+import de.mtg.jzlint.EffectiveDate;
+import de.mtg.jzlint.JavaLint;
+import de.mtg.jzlint.Lint;
+import de.mtg.jzlint.LintResult;
+import de.mtg.jzlint.Source;
+import de.mtg.jzlint.Status;
+import de.mtg.jzlint.utils.Utils;
+
+@Lint(
+        name = "e_subscribers_shall_have_crl_distribution_points",
+        description = "cRLDistributionPoints SHALL be present.",
+        citation = "7.1.2.3.b",
+        source = Source.CABF_SMIME_BASELINE_REQUIREMENTS,
+        effectiveDate = EffectiveDate.SMIME_BR_1_0_DATE)
+public class SubscribersShallHaveCrlDistributionPoints implements JavaLint {
+
+    @Override
+    public LintResult execute(X509Certificate certificate) {
+
+        if (!Utils.hasCRLDPExtension(certificate)) {
+            return LintResult.of(Status.ERROR, "SMIME certificate contains zero CRL distribution points");
+        }
+
+        byte[] rawCRLDPs = certificate.getExtensionValue(Extension.cRLDistributionPoints.getId());
+
+        if (rawCRLDPs == null) {
+            return LintResult.of(Status.FATAL);
+        }
+
+        CRLDistPoint cRLDPs;
+        try {
+            cRLDPs = CRLDistPoint.getInstance(ASN1OctetString.getInstance(rawCRLDPs).getOctets());
+        } catch (Exception ex) {
+            return LintResult.of(Status.FATAL);
+        }
+
+        DistributionPoint[] distributionPoints = cRLDPs.getDistributionPoints();
+
+        if (distributionPoints == null || distributionPoints.length == 0) {
+            return LintResult.of(Status.ERROR, "SMIME certificate contains zero CRL distribution points");
+        }
+
+        return LintResult.of(Status.PASS);
+    }
+
+    @Override
+    public boolean checkApplies(X509Certificate certificate) {
+        return Utils.isSubscriberCert(certificate);
+    }
+
+}
---
+++ jzlint/src/test/java/de/mtg/jzlint/lints/cabf_smime_br/SubscribersShallHaveCrlDistributionPointsTest.java
@@ -0,0 +1,28 @@
+package de.mtg.jzlint.lints.cabf_smime_br;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import de.mtg.jzlint.LintTest;
+import de.mtg.jzlint.LintTestExtension;
+import de.mtg.jzlint.Status;
+
+@ExtendWith(LintTestExtension.class)
+class SubscribersShallHaveCrlDistributionPointsTest {
+
+    @LintTest(
+            name = "e_subscribers_shall_have_crl_distribution_points",
+            filename = "smime/subscriber_with_crl_distribution_points.pem",
+            expectedResultStatus = Status.PASS,
+            certificateDescription = "pass - cert with a CRL distribution point")
+    void testCase01() {
+    }
+
+    @LintTest(
+            name = "e_subscribers_shall_have_crl_distribution_points",
+            filename = "smime/subscriber_no_crl_distribution_points.pem",
+            expectedResultStatus = Status.ERROR,
+            certificateDescription = "error - cert without a CRL distribution point")
+    void testCase02() {
+    }
+
+}
