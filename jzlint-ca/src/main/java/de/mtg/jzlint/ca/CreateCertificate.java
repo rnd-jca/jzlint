54f3facb4ba22e18e47a4bfa2b06fb4401f855d2
mtgag <githreg@mtg.de>
2024-02-01 07:52:17 +0100

    added module to easily create certificates that can be used in tests of zlint and jzlint

---
+++ jzlint-ca/src/main/java/de/mtg/jzlint/ca/CreateCertificate.java
@@ -0,0 +1,145 @@
+package de.mtg.jzlint.ca;
+
+
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.cert.X509Certificate;
+import java.security.spec.AlgorithmParameterSpec;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.Random;
+
+import org.bouncycastle.asn1.ASN1Encoding;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AccessDescription;
+import org.bouncycastle.asn1.x509.AuthorityInformationAccess;
+import org.bouncycastle.asn1.x509.AuthorityKeyIdentifier;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
+public class CreateCertificate {
+
+    public static final String SHA_256_WITH_ECDSA = "SHA256WithECDSA";
+
+    public static void main(String[] args) throws Exception {
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        X500Name caIssuerDN = new X500Name("CN=Lint CA, O=Lint, C=DE");
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
+        AlgorithmParameterSpec algParSpec = ECNamedCurveTable.getParameterSpec("prime256v1");
+        keyPairGenerator.initialize(algParSpec);
+
+        KeyPair rootKeyPair = keyPairGenerator.generateKeyPair();
+        PublicKey rootPublicKey = rootKeyPair.getPublic();
+        PrivateKey rootPrivateKey = rootKeyPair.getPrivate();
+
+        //SubjectPublicKeyInfo spki = SubjectPublicKeyInfo.getInstance(rootPublicKey);
+        //BigInteger serialNumber = new BigInteger(96, new Random());
+        //Date notBefore = Date.from(LocalDateTime.now().minusDays(1).atZone(ZoneId.systemDefault()).toInstant());
+        //Date noteAfter = Date.from(LocalDateTime.now().plusYears(5).atZone(ZoneId.systemDefault()).toInstant());
+        //
+        //X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(caIssuerDN, serialNumber, notBefore, noteAfter, caIssuerDN, spki);
+        //
+        //AuthorityKeyIdentifier aki = new JcaX509ExtensionUtils().createAuthorityKeyIdentifier(rootPublicKey);
+        //SubjectKeyIdentifier ski = new JcaX509ExtensionUtils().createSubjectKeyIdentifier(rootPublicKey);
+        //
+        //Extension akie = new Extension(Extension.authorityKeyIdentifier, false, aki.toASN1Primitive().getEncoded(ASN1Encoding.DER));
+        //Extension skie = new Extension(Extension.subjectKeyIdentifier, false, ski.toASN1Primitive().getEncoded(ASN1Encoding.DER));
+        //
+        //BasicConstraints bc = new BasicConstraints(true);
+        //Extension basicConstraints = new Extension(Extension.basicConstraints, true, bc.toASN1Primitive().getEncoded(ASN1Encoding.DER));
+        //
+        //certificateBuilder.addExtension(akie);
+        //certificateBuilder.addExtension(skie);
+        //certificateBuilder.addExtension(basicConstraints);
+        //
+        //JcaContentSignerBuilder jcaContentSignerBuilder = new JcaContentSignerBuilder(SHA_256_WITH_ECDSA);
+        //jcaContentSignerBuilder = jcaContentSignerBuilder.setProvider(BouncyCastleProvider.PROVIDER_NAME);
+        //ContentSigner contentSigner = jcaContentSignerBuilder.build(rootPrivateKey);
+        //X509CertificateHolder x509CertificateHolder = certificateBuilder.build(contentSigner);
+        //
+        //JcaX509CertificateConverter certificateConverter = new JcaX509CertificateConverter().setProvider(BouncyCastleProvider.PROVIDER_NAME);
+        //X509Certificate roootCaCertificate = certificateConverter.getCertificate(x509CertificateHolder);
+
+        X509Certificate testCertificate = createTestCertificate(rootPublicKey, rootPrivateKey, caIssuerDN);
+        String name = "aiaWithIP";
+        String nameDER = String.format("%s.der", name);
+        String namePEM = String.format("%s.pem", name);
+
+        Files.write(Paths.get(nameDER), testCertificate.getEncoded());
+
+        System.out.println(String.format("openssl x509 -inform DER -outform PEM -in %s -out %s -text", nameDER, namePEM));
+
+    }
+
+    /**
+     * test certificate for lint_sub_cert_aia_contains_internal_names.go
+     *
+     * @param caPublicKey
+     * @param caPrivateKey
+     * @param issuerDN
+     *
+     * @return
+     *
+     * @throws Exception
+     */
+    private static X509Certificate createTestCertificate(PublicKey caPublicKey, PrivateKey caPrivateKey, X500Name issuerDN) throws Exception {
+
+        BigInteger serialNumber = new BigInteger(96, new Random());
+        ZonedDateTime notBefore = ZonedDateTime.of(2023, 9, 1, 0, 0, 0, 0, ZoneId.of("UTC"));
+        Date notBeforeDate = Date.from(notBefore.toInstant());
+        Date noteAfterDate = Date.from(notBefore.plusYears(1).toInstant());
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
+        AlgorithmParameterSpec algParSpec = ECNamedCurveTable.getParameterSpec("prime256v1");
+        keyPairGenerator.initialize(algParSpec);
+
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        SubjectPublicKeyInfo subjectPublicKeyInfo = SubjectPublicKeyInfo.getInstance(publicKey.getEncoded());
+
+        X500Name subjectDN = new X500Name("CN=Certificate, O=Lint, C=DE");
+
+        AuthorityKeyIdentifier aki = new JcaX509ExtensionUtils().createAuthorityKeyIdentifier(caPublicKey);
+        SubjectKeyIdentifier ski = new JcaX509ExtensionUtils().createSubjectKeyIdentifier(publicKey);
+        Extension akie = new Extension(Extension.authorityKeyIdentifier, false, aki.toASN1Primitive().getEncoded(ASN1Encoding.DER));
+        Extension skie = new Extension(Extension.subjectKeyIdentifier, false, ski.toASN1Primitive().getEncoded(ASN1Encoding.DER));
+
+        AccessDescription accessDescription = new AccessDescription(AccessDescription.id_ad_ocsp,
+                new GeneralName(GeneralName.uniformResourceIdentifier, "http://198.51.100.42/ocsp"));
+        AuthorityInformationAccess aia = new AuthorityInformationAccess(accessDescription);
+        Extension aiaExtension = new Extension(Extension.authorityInfoAccess, false, aia.toASN1Primitive().getEncoded(ASN1Encoding.DER));
+
+        X509v3CertificateBuilder certificateBuilder =
+                new X509v3CertificateBuilder(issuerDN, serialNumber, notBeforeDate, noteAfterDate, subjectDN, subjectPublicKeyInfo);
+        certificateBuilder.addExtension(akie);
+        certificateBuilder.addExtension(skie);
+        certificateBuilder.addExtension(aiaExtension);
+        JcaContentSignerBuilder jcaContentSignerBuilder = new JcaContentSignerBuilder(SHA_256_WITH_ECDSA);
+        ContentSigner contentSigner = jcaContentSignerBuilder.setProvider(BouncyCastleProvider.PROVIDER_NAME).build(caPrivateKey);
+        X509CertificateHolder x509CertificateHolder = certificateBuilder.build(contentSigner);
+
+        return new JcaX509CertificateConverter().setProvider(BouncyCastleProvider.PROVIDER_NAME).getCertificate(x509CertificateHolder);
+
+    }
+
+}
